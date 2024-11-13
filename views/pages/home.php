2c8d3b467ceaf07a71f8d4a291473e335f9da53f
POINTBREAK22 <zbrozhek13@gmail.com>
2024-11-13 09:40:28 +0000

    start

---
+++ views/pages/home.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var App\Kernel\View\View $view */
+
+$view->component('header', ['title' => "Home"]);
+?>
+
+    <h3>Home page</h3>
+
+    <h5>work</h5>
+
+<?php
+$view->component('footer'); ?>
